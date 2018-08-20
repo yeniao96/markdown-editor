dfc2d9a0975860ff3b829ff0fa2dde5fed1dc6c5
guzijie <guzijie@kedacom.com>
2018-08-20 16:45:52 +0800

    the first version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+
 # markdown-editor
-a web markdown editor
+
+markdown-editor是一款 markdown web编辑器。
+
+## 主要快捷功能
+
+* 生成文档目录
+* 字体加粗
+* 字体倾斜
+* 插入链接
+* 插入图片
+* 插入列表
+* 插入各级标题
+* 全屏切换
---
+++ js/tool.js
@@ -0,0 +1,163 @@
+/**
+ * 展示目录
+ */
+function showCatalog(){
+
+    if(document.getElementById('md-catalog').style.display){
+        document.getElementById('md-catalog').style.display = ''
+    }else{
+        createDiretory('markdown-preview', 'md-catalogContent')
+        document.getElementById('md-catalog').style.display = 'block'
+    }
+}
+
+/**
+ * 创建目录
+ * @param contentId
+ * @param directoryId
+ */
+function createDiretory(contentId, directoryId) {
+
+    var minLevel = 6;
+    $("#" + directoryId).html('');
+    $('#' + contentId).children().each(function (index, node) {
+
+        var tagName = $(this).get(0).tagName;
+
+        if(tagName.substr(0,1).toLowerCase() === "h"){
+            var contentH = $(this).html();
+            var level=tagName.substr(1,1);
+            if(level <= minLevel){
+                minLevel = level;
+            }else{
+                //缩进的长度
+                for(var i=1;i<level;i++){
+                    contentH="&nbsp;&nbsp;" + contentH;
+                }
+            }
+            //添加id
+            var hID = "Title-"+tagName+"-"+index.toString();
+            $(this).attr("id",hID);
+            hID = "#" + hID;
+            //在目标DIV中添加 li
+            $("#" + directoryId).append("<li><a href='"+hID+"'>"+contentH+"</a></li>");
+        }
+
+    })
+
+    // $("[href='#']").click(function(){
+    //
+    //     $('#' + contentId).animate({scrollTop:0},300);
+    //     return false;
+    //
+    // });
+
+    $("[href^='#Title']").click(function(){
+
+        selectedOption(directoryId, $(this)[0])
+        var var_href = $(this).attr("href");
+        var len_href = var_href.length;
+        var has_mao = var_href.search("#");
+        if ( has_mao === '-1' ) {
+            return;
+        }
+
+        var index_mao = var_href.lastIndexOf("#");
+        var sub_href = var_href.substr(index_mao,len_href-index_mao);
+        console.log($('#' + contentId).scrollTop())
+        var scrollTop = $('#' + contentId).scrollTop() + $(sub_href).offset().top - 40 + 'px'
+        $('#' + contentId).animate({scrollTop:scrollTop},300);
+        return false;
+
+    });
+}
+
+/**
+ * 选中样式
+ * @param id
+ * @param node
+ */
+function selectedOption(id, node) {
+
+    $('#' + id).children().each(function (index, node) {
+
+        if(node.classList.contains('selected')){
+
+            node.classList.remove('selected');
+
+        }
+
+    });
+    node.parentNode.classList.add('selected');
+
+}
+
+/**
+ * 保存文件
+ */
+function saveText() {
+
+    var a = document.createElement('a');
+    a.setAttribute('href','data:text/html;gb2312,'+ md_editor.getValue());
+    a.setAttribute('download', 'demo.md');
+    a.setAttribute('target','_blank');
+    a.style.display="none";
+    $('#markdown-editor').append(a);
+    a.click();
+
+}
+
+/**
+ * 快捷输入
+ * @param value
+ * @param type
+ */
+function insertText(value,type){
+
+    var text =  md_editor.session.getTextRange(md_editor.getSelectionRange());
+    md_editor.insert(value.replace('?',text));
+    if(type === 'code'){
+
+        var lineNumber = md_editor.selection.getCursor();
+        md_editor.gotoLine(lineNumber);
+
+    }
+
+}
+
+/**
+ * 插入图片
+ */
+function insertImage() {
+
+    var str =
+    md_editor.insert(str);
+
+}
+
+
+/**
+ * 全屏展示
+ */
+function fullScreen() {
+
+    if ($('#editor-column')[0].style.width !== '100%') {
+
+        $('#editor-column')[0].style.width = '100%';
+        $('#preview-column')[0].style.width = '0%';
+        $('#preview-column')[0].style.paddingLeft = '0px';
+        $('#editor-toolbar')[0].style.width = '100%'
+        md_editor.setOption("wrap", "150")
+
+    }else{
+
+        $('#editor-column')[0].style.width = '50%';
+        $('#preview-column')[0].style.width = '50%';
+        $('#editor-toolbar')[0].style.width = '50%';
+        md_editor.setOption("wrap", "free")
+
+    }
+
+}
+
+
